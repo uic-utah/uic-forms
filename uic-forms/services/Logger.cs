e014cd9f3e3a9f37c999e749c3df14fee34b56ec
steveoh <sgourley@utah.gov>
2018-04-12 19:35:43 -0600

    add logging

---
+++ uic-forms/services/Logger.cs
@@ -0,0 +1,39 @@
+﻿namespace uic_forms.services
+{
+    using System;
+
+    internal class Logger
+    {
+        private readonly bool _verbose;
+
+        internal Logger(bool verbose)
+        {
+            _verbose = verbose;
+        }
+
+        internal void AlwaysWrite(string format, params object[] args)
+        {
+            Console.Write(" # ");
+            Console.Write(format, args);
+
+            if (format.EndsWith("."))
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            Console.Write("...");
+            Console.WriteLine();
+        }
+
+        internal void Write(string format, params object[] args)
+        {
+            if (!_verbose)
+            {
+                return;
+            }
+
+            AlwaysWrite(format, args);
+        }
+    }
+}
