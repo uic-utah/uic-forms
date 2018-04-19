3c819ff297d52a2aa42f3bd46f3b324b33eb38e0
steveoh <sgourley@utah.gov>
2018-04-18 19:13:30 -0600

    there's a note about october 1 need to clarify dates

--- uic-forms/models/CliOptions.cs
+++ uic-forms/models/CliOptions.cs
@@ -5,7 +5,7 @@
     internal class CliOptions
     {
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; } = DateTime.Today;
+        public DateTime EndDate { get; set; } = new DateTime(DateTime.Now.Year, 10, 1);
         public bool Verbose { get; set; }
         public string TemplateLocation { get; set; }
         public string OutputPath { get; set; } = "c:\\temp";
