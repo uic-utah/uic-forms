6d5f053c4913ea757e9c7836fa6ae16e229d1fd4
steveoh <sgourley@utah.gov>
2018-10-18 11:42:47 -0600

    chore: remove unused variable

--- uic-forms/models/CliOptions.cs
+++ uic-forms/models/CliOptions.cs
@@ -7,7 +7,6 @@
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public bool Verbose { get; set; }
-        public string TemplateLocation { get; set; }
         public string OutputPath { get; set; }
         public string Source { get; set; }
         public string Password { get; set; }
